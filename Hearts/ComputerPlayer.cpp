f008aeaf4564021c800362e4a525a4d6a2b22d7d
Doran Williams <116115557+Doran-28@users.noreply.github.com>
2023-01-28 12:21:52 -0500

    Delete Hearts directory

--- Hearts/ComputerPlayer.cpp
+++
@@ -1,7 +0,0 @@
-#include "ComputerPlayer.hpp"
-
-static int count = 1;
-// Count if to keep track of how many computer players there is
-
-ComputerPlayer::ComputerPlayer() : Player("Computer " + std::to_string(count++)) {}
-
